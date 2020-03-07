598d648f81a4d44e826bcdd2735d3a5cce8b7d12
Begin Sergey <begins.1986@mail.ru>
2020-03-07 13:15:00 +0300

    feat: add code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,47 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!Array.isArray(array) || array.length == 0) {
+    console.log(0);
+    return 0;
+} else {
+    var minValue = array[0];
+    for (let i = 0; i < array.length; i++) {
+        if (minValue > array[i]) {
+            minValue = array[i];
+        }
+    }
+    console.log(minValue);
+    return minValue;
+}
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!Array.isArray(array) || array.length == 0) {
+    console.log(0);
+    return 0;
+} else {
+    var maxValue = array[0];
+    for (let i = 0; i < array.length; i++) {
+        if (maxValue < array[i]) {
+            maxValue = array[i];
+        }
+    }
+    console.log(maxValue);
+    return maxValue;
+}
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!Array.isArray(array) || array.length == 0) {
+    console.log(0);
+    return 0;
+} else {
+    var Value = 0;
+    for (let i = 0; i < array.length; i++) {
+        Value += array[i];
+    }
+    var avgValue = Value / array.length;
+    console.log(avgValue);
+    return avgValue;
+}
 }
